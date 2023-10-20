fb2789a30306ada9d7c3fd6fcdfc2f7e9947cc04
ren0509 <130971107+ren0509@users.noreply.github.com>
2023-10-20 16:23:53 +0900

    next.js

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,43 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
-
-const inter = Inter({ subsets: ['latin'] })
+import styles from '@/styles/Home.module.css';
 
 export default function Home() {
-    const subtitle = 'アウトプットしていくサイト'
+    const subtitle = 'アウトプットしていくサイト';
     return (
-                <header>HEADER</header>
-    <main>
-        <div className='hero'>
-            <h1 style={{ color: 'red',fontSize: '80'  }}>
-                CUBE
-            </h1>
-            <p>{subtitle}</p>    
+        <div>
+            <header>HEADER</header>
+            <main>
+                <div className={styles.hero}>
+                    <h1 style={{ color: 'red', fontSize: '80px' }}>
+                        CUBE
+                    </h1>
+                    <p>{subtitle}</p>
+                </div>
+            </main>
+            <footer>FOOTER</footer>
         </div>
-    <main/>
-                <footer>HEADER</footer>
+    );
+}
+
+function EachPost({ title, url}) {
+    return (
+        <article>
+            <a href={url}>
+                <h3>{title}</h3>
+            </a>
+        </article>
+    )
+}
+
+export default function Posts() {
+    const props1 = { title: '記事のタイトル１', ult: 'post1.html' }
+    const props2 = { title: '記事のタイトル２', ult: 'post2.html' }
+
+
+return (
+    <section>
+        <h2>おすすめの記事</h2>    
+        <EachPost {...props1} />
+        <EachPost {...props2} />
+    </section>
     )
 }
