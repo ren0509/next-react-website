df2df5da5ff76a77777170d8bdf0a44300536863
ren0509 <130971107+ren0509@users.noreply.github.com>
2023-11-17 16:28:27 +0900

    nextjs2.5

--- pages/index.js
+++ pages/index.js
@@ -1,43 +1,64 @@
-import styles from '@/styles/Home.module.css';
+import Header from '@/components/header'
+import Hero from '@/components/hero'
+import Footer from '@/components/footer' 
 
 export default function Home() {
-    const subtitle = 'アウトプットしていくサイト';
     return (
-        <div>
-            <header>HEADER</header>
+        <>
+            <Header />
             <main>
-                <div className={styles.hero}>
-                    <h1 style={{ color: 'red', fontSize: '80px' }}>
-                        CUBE
-                    </h1>
-                    <p>{subtitle}</p>
-                </div>
+                <Hero />
             </main>
-            <footer>FOOTER</footer>
-        </div>
-    );
-}
 
-function EachPost({ title, url}) {
-    return (
-        <article>
-            <a href={url}>
-                <h3>{title}</h3>
-            </a>
-        </article>
+            <Footer />
+
+        </>
     )
 }
+    
+    
+    
+    
+    
+    
+    
+    
+    //const subtitle = 'アウトプットしていくサイト';
+    // return (
+        // <div>
+            // <header>HEADER</header>
+            // <main>
+                // <div className={styles.hero}>
+                    // <h1 style={{ color: 'red', fontSize: '80px' }}>
+                        // CUBE
+                    // </h1>
+                    // <p>{subtitle}</p>
+                // </div>
+            // </main>
+            // <footer>FOOTER</footer>
+        // </div>
+    // );
 
-export default function Posts() {
-    const props1 = { title: '記事のタイトル１', ult: 'post1.html' }
-    const props2 = { title: '記事のタイトル２', ult: 'post2.html' }
+// function EachPost({ title, url}) {
+//     return (
+//         <article>
+//             <a href={url}>
+//                 <h3>{title}</h3>
+//             </a>
+//         </article>
+//     )
+// }
+
+// export  function Posts() {
+//     const props1 = { title: '記事のタイトル１', ult: 'post1.html' }
+//     const props2 = { title: '記事のタイトル２', ult: 'post2.html' }
 
 
-return (
-    <section>
-        <h2>おすすめの記事</h2>    
-        <EachPost {...props1} />
-        <EachPost {...props2} />
-    </section>
-    )
-}
+// return (
+//     <section>
+//         <h2>おすすめの記事</h2>    
+//         <EachPost {...props1} />
+//         <EachPost {...props2} />
+//     </section>
+//     )
+// }
