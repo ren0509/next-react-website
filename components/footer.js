9937200c7bc476aeb73896fabb50d88f723f3fb3
ren0509 <130971107+ren0509@users.noreply.github.com>
2023-12-01 16:31:00 +0900

    next.js 4.5 117

--- components/footer.js
+++ components/footer.js
@@ -1,9 +1,15 @@
+import Container from 'components/container'
 import Logo from './logo'
+import styles from 'styles/footer.module.css'
 
 export default function Footer() {
     return (
-        <footer>
-            <Logo />
+        <footer className={styles.wrapper}>
+            <Container>
+            <div className={styles.flexContainer}>
+                <Logo />
+                </div>
+            </Container>    
         </footer>
     )
 }
--- components/layout.js
+++ components/layout.js
@@ -4,14 +4,14 @@ import Footer from 'components/footer'
 export default function Layout({ children }) {
     return (
         <>
-          <Header />
-            
-            <main>{children}</main>        
-            
-          <Footer />
-          
-        
+        <Header />
+
+            <main>{children}</main>
+
+        <Footer />
+
+
         </>
     )
-    
+
 }
